db60e4ddee5b5cfb5fe17aeea603201a4e0c0992
fr0gs <elf_elegua@yahoo.com>
2017-10-26 17:34:21 +0200

    Get stats of a given service

--- mudockerstats/main.py
+++ mudockerstats/main.py
@@ -7,8 +7,7 @@ import jsonapi_requests
 from aiohttp import web
 from aiohttp.client_exceptions import ClientConnectionError
 from aiosparql.client import SPARQLClient
-from aiosparql.syntax import (IRI, RDF, Node, RDFTerm, Triples, escape_any,
-                              escape_string)
+from aiosparql.syntax import IRI, escape_string
 
 logger = logging.getLogger(__name__)
 
@@ -36,8 +35,81 @@ class Application(web.Application):
         return self._sparql
 
 
+    async def get_service_stats(self, pipeline, service_name):
+        """
+        Get the CPU & Memory stats of the most recent stats event
+        linked to a given service inside a pipeline.
+
+        Arguments:
+            pipeline: name of the pipeline containing the service
+            service_name: service name
+
+        Returns: An object with the form:
+            {
+            'systemCpuUsage':{
+               'type':'typed-literal',
+               'datatype':'http://www.w3.org/2001/XMLSchema#integer',
+               'value':'93887870000000'
+            },
+            'totalUsage':{
+               'type':'typed-literal',
+               'datatype':'http://www.w3.org/2001/XMLSchema#integer',
+               'value':'3003160973'
+            },
+            (...etc...)
+         }
+
+        """
+        stats_service_name = "/{}_{}_1".format(pipeline, service_name)
+        result = await self.sparql.query("""
+            PREFIX swarmui: <http://swarmui.semte.ch/vocabularies/core/>
+            SELECT DISTINCT ?systemCpuUsage ?totalUsage ?presystemCpuUsage ?pretotalUsage ?memoryUsage ?memoryLimit
+            FROM {{graph}}
+            WHERE {
+                ?stats a swarmui:Stats .
+                ?stats swarmui:name {{name}} .
+                ?stats swarmui:read ?readdate .
+                ?stats swarmui:cpuStats ?cpuStats .
+                ?stats swarmui:precpuStats ?precpuStats .
+                ?stats swarmui:memoryStats ?memoryStats .
+
+                ?memoryStats swarmui:usage ?memoryUsage .
+                ?memoryStats swarmui:limit ?memoryLimit .
+
+                ?cpuStats swarmui:systemCpuUsage ?systemCpuUsage .
+                ?cpuStats swarmui:cpuUsage ?cpuUsage .
+                ?cpuUsage swarmui:totalUsage ?totalUsage .
+
+                ?precpuStats swarmui:systemCpuUsage ?presystemCpuUsage .
+                ?precpuStats swarmui:cpuUsage ?precpuUsage .
+                ?precpuUsage swarmui:totalUsage ?pretotalUsage .
+            }
+            ORDER BY DESC(?readdate)
+            LIMIT 1
+            """, name=escape_string(stats_service_name))
+        return result['results']['bindings'][0]
+
 
     async def handle_get_service_stats(self, request):
+        """
+        Handle a get request and return the stats of a given
+        service in a JSON-API compliant format
+        
+        Arguments:
+            request: the request object
+        """
+        try:
+            pipeline = request.GET['pipeline']
+            service_name = request.GET['service']
+            service_stats = await self.get_service_stats(pipeline, service_name)
+            logger.info(service_stats)
+            return web.Response(text="zi")
+        except KeyError:
+            raise web.HTTPInternalServerError(body=json.dumps({
+                "status": 500,
+                "title": "Wrong query parameters",
+                "detail": "Wrong query parameters"
+            }))
         return
 
     async def handle_get_pipeline_stats(self, request):
@@ -45,5 +117,5 @@ class Application(web.Application):
 
 
 app = Application()
-app.router.add_get('/service/stats', app.handle_get_service_stats)
-app.router.add_get('/pipeline/stats', app.handle_get_pipeline_stats)
+app.router.add_get('/stats/service', app.handle_get_service_stats)
+app.router.add_get('/stats/pipeline', app.handle_get_pipeline_stats)
