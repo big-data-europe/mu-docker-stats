b9c27c5ae0b5144eaed1d9dadc46bea536bcefcb
fr0gs <elf_elegua@yahoo.com>
2017-11-08 17:51:13 +0100

    Query returns for multiple services

--- mudockerstats/main.py
+++ mudockerstats/main.py
@@ -23,8 +23,8 @@ class Application(web.Application):
     """
     The main application class.
     """
-    sparql_timeout = 60
-    run_command_timeout = 60
+    sparql_timeout = 600
+    run_command_timeout = 600
 
     @property
     def sparql(self):
@@ -39,7 +39,7 @@ class Application(web.Application):
         return self._sparql
 
 
-    async def get_service_stats(self, pipeline, service_name):
+    async def get_service_stats(self, pipelines, service_names):
         """
         Get the CPU & Memory stats of the most recent stats event
         linked to a given service inside a pipeline.
@@ -69,36 +69,51 @@ class Application(web.Application):
          }
 
         """
-        stats_service_name = "/{}_{}_1".format(pipeline.lower(), service_name) if pipeline is not None else "/{}".format(service_name)
+        stats_service_names = ['/{}_{}_1'.format(pair[0].lower(), pair[1]) for pair in zip(pipelines, service_names)]
+        escaped_result = ", ".join(map(escape_string, stats_service_names))
         result = await self.sparql.query("""
             PREFIX swarmui: <http://swarmui.semte.ch/vocabularies/core/>
-            SELECT DISTINCT ?systemCpuUsage ?totalUsage ?presystemCpuUsage ?pretotalUsage ?memoryUsage ?memoryLimit count(?perCpuUsage) as ?countPerCpuUsage
-            FROM {{graph}}
-            WHERE {
+            SELECT DISTINCT ?name ?readdate ?systemCpuUsage ?totalUsage ?presystemCpuUsage ?pretotalUsage count(?perCpuUsage) as ?countPerCpuUsage ?memoryUsage ?memoryLimit
+            WHERE
+            {
+                {
+                    SELECT ?name MAX(?readdate) as ?readdate
+                    WHERE
+                    {
+                        {
+                            SELECT DISTINCT ?name ?readdate 
+                            FROM {{graph}}
+                            WHERE {
+                                ?stats a swarmui:Stats .
+                                ?stats swarmui:name ?name .
+                                FILTER(?name IN({{escaped_result}}))
+                                ?stats swarmui:read ?readdate .
+                            }
+                            ORDER BY ?name
+                        }
+                    }
+                    GROUP BY ?name
+                }
                 ?stats a swarmui:Stats .
-                ?stats swarmui:name {{name}} .
                 ?stats swarmui:read ?readdate .
                 ?stats swarmui:cpuStats ?cpuStats .
                 ?stats swarmui:precpuStats ?precpuStats .
                 ?stats swarmui:memoryStats ?memoryStats .
-
                 ?memoryStats swarmui:usage ?memoryUsage .
                 ?memoryStats swarmui:limit ?memoryLimit .
-
                 ?cpuStats swarmui:systemCpuUsage ?systemCpuUsage .
                 ?cpuStats swarmui:cpuUsage ?cpuUsage .
                 ?cpuUsage swarmui:totalUsage ?totalUsage .
                 ?cpuUsage swarmui:percpuUsage ?perCpuUsage .
-
                 ?precpuStats swarmui:systemCpuUsage ?presystemCpuUsage .
                 ?precpuStats swarmui:cpuUsage ?precpuUsage .
                 ?precpuUsage swarmui:totalUsage ?pretotalUsage .
             }
-            ORDER BY DESC(?readdate)
-            LIMIT 1
-            """, name=escape_string(stats_service_name))
-        stats = result['results']['bindings'][0]
-        return { stat: stats[stat]['value'] for stat in stats }
+            """, escaped_result=escaped_result)
+        stats = result['results']['bindings']
+        names = { stat['name']['value']: [ { stat['name']['value']: stats[stat]['value'] for stat in stats if stat is not 'name'} ] for stat in stats }
+        logger.info(names)
+        return { stat['name']['value']: stats[stat]['value'] for stat in stats }
 
 
     async def get_json_stats(self, service_stats):
@@ -154,9 +169,9 @@ class Application(web.Application):
             request: the request object
         """
         try:
-            pipeline = request.GET['pipeline']
-            service_name = request.GET['service']
-            service_stats = await self.get_service_stats(pipeline, service_name)
+            pipelines = request.GET['pipelines'].split(',')
+            service_names = request.GET['services'].split(',')
+            service_stats = await self.get_service_stats(pipelines, service_names)
             json_response = await self.get_json_stats(service_stats)
             return web.Response(body=json_response)
         except KeyError:
@@ -172,5 +187,4 @@ class Application(web.Application):
 
 
 app = Application()
-app.router.add_get('/stats/service', app.handle_get_service_stats)
-app.router.add_get('/stats/pipeline', app.handle_get_pipeline_stats)
+app.router.add_get('/stats', app.handle_get_service_stats)
